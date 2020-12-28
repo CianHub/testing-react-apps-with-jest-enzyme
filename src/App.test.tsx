b92743324deeb5ac25a83457dbe5e05133e44189
Cian O'Gralaigh <ihatecian@gmail.com>
2020-12-28 19:50:17 +0000

    Add tests and functionality for click counter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,16 +6,50 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+/**
+ * Factory function create a shallow wrapper for app component
+ * @function setup
+ * @returns {Enzyme.ShallowWrapper}
+ */
+const setup = (): Enzyme.ShallowWrapper => shallow(<App />);
+
+const findByTestAttr = (
+  wrapper: Enzyme.ShallowWrapper,
+  selector: string
+): Enzyme.ShallowWrapper<
+  Enzyme.HTMLAttributes,
+  any,
+  React.Component<{}, {}, any>
+> => wrapper.find(`[data-test='${selector}']`);
+
 test('renders app without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
-test('renders button', () => {});
+test('renders button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'component-button');
+  expect(button.length).toBe(1);
+});
 
-test('renders counter display', () => {});
+test('renders counter display', () => {
+  const wrapper = setup();
+  const display = findByTestAttr(wrapper, 'component-display');
+  expect(display.length).toBe(1);
+});
 
-test('counter starts at 0', () => {});
+test('counter starts at 0', () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, 'component-count').text();
+  expect(count).toBe('0');
+});
 
-test('increments counter on click', () => {});
+test('increments counter on click', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'component-button');
+  button.simulate('click');
+  const count = findByTestAttr(wrapper, 'component-count').text();
+  expect(count).toBe('1');
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return <div data-test="component-app" className="App"></div>;
-}
+const App: React.FC = () => {
+  const [state, setState] = useState<number>(0);
+  return (
+    <div data-test="component-app" className="App">
+      <h1 data-test="component-display">
+        Counter:&nbsp;<span data-test="component-count">{state}</span>
+      </h1>
+      <button
+        data-test="component-button"
+        type="button"
+        onClick={() => setState(state + 1)}
+      >
+        Increment Counter
+      </button>
+    </div>
+  );
+};
 
 export default App;
